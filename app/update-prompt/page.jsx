10ff944709beb2ea6f3c39297cf27b6d55eff688
Yohannis41 <119919990+Yohannis41@users.noreply.github.com>
2024-12-23 02:23:19 -1200

    update suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, Suspense } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@component/Form";
 
@@ -16,11 +16,7 @@ const UpdatePrompt = () => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
+      setPost({ prompt: data.prompt, tag: data.tag });
     };
 
     if (promptId) getPromptDetails();
@@ -51,9 +47,8 @@ const UpdatePrompt = () => {
     }
   };
 
-  // Wrap the Form component with Suspense to manage client-side loading
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense>
       <Form
         type="Edit"
         post={post}
